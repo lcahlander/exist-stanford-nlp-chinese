4ca210ddf414a338b128f6afa56fd0aa89baf5c9
lcahlander <loren.cahlander@gmail.com>
2020-01-24 18:15:34 -0500

    Copied the english version over and changed for chinese

---
+++ src/main/java/org/exist/xquery/nlp/chinese/StanfordNLPModule.java
@@ -0,0 +1,65 @@
+/*
+ *   exist-stanford-nlp-chinese: XQuery module to integrate the stanford named entity
+ *   extraction library with eXist-db.
+ *   Copyright (C) 2013 Wolfgang Meier and contributors
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.exist.xquery.nlp.chinese;
+
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Integrates the Stanford named entity recognition system.
+ *
+ * @author Wolfgang
+ */
+public class StanfordNLPModule extends AbstractInternalModule {
+
+    public final static String NAMESPACE_URI = "http://exist-db.org/xquery/stanford-nlp/chinese";
+    public final static String PREFIX = "nlpzh";
+
+    public final static FunctionDef[] functions = {
+        new FunctionDef(StanfordNLPFunction.signatures[0], StanfordNLPFunction.class),
+        new FunctionDef(StanfordNLPFunction.signatures[1], StanfordNLPFunction.class)
+    };
+
+    public StanfordNLPModule(Map<String, List<? extends Object>> parameters) {
+        super(functions, parameters, false);
+    }
+
+    @Override
+    public String getNamespaceURI() {
+        return NAMESPACE_URI;
+    }
+
+    @Override
+    public String getDefaultPrefix() {
+        return PREFIX;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Stanford Natural Language Processing for Chinese";
+    }
+
+    @Override
+    public String getReleaseVersion() {
+        return null;
+    }
+}
---
+++ src/test/java/org/exist/xquery/nlp/chinese/StanfordNLPModuleTest.java
@@ -0,0 +1,39 @@
+package org.exist.xquery.nlp.chinese;
+
+
+import org.exist.EXistException;
+import org.exist.security.PermissionDeniedException;
+import org.exist.storage.BrokerPool;
+import org.exist.storage.DBBroker;
+import org.exist.test.ExistEmbeddedServer;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQuery;
+import org.exist.xquery.value.Sequence;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class StanfordNLPModuleTest {
+
+    @ClassRule
+    public static ExistEmbeddedServer existEmbeddedServer = new ExistEmbeddedServer(false, true);
+
+    @Test
+    public void helloWorld() throws XPathException, PermissionDeniedException, EXistException {
+        assertTrue(true);
+    }
+
+    private Sequence executeQuery(final String xquery) throws EXistException, PermissionDeniedException, XPathException {
+        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
+        final XQuery xqueryService = pool.getXQueryService();
+
+        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()))) {
+            return xqueryService.execute(broker, xquery, null);
+        }
+    }
+}
